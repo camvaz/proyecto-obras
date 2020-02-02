346799e4b274d471941e424db98983c7a1263b28
camvaz <camposvazquezv@gmail.com>
2020-02-02 08:59:44 -0600

    routing fixes

--- web-app/src/app/app-routing.module.ts
+++ web-app/src/app/app-routing.module.ts
@@ -3,8 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ComponentesResueltosComponent } from './componentes-resueltos/componentes-resueltos.component';
 const routes: Routes = [
-  {path:'', component:ComponentesResueltosComponent},
-  {path:'login', component:LoginComponent}
+  {path:'', component:LoginComponent},
+  {path:'login', component:ComponentesResueltosComponent}
 ];
 
 @NgModule({
