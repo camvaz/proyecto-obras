6d31963b3498982b5e6f06edf130d3d5b7221ae7
Victor Campos <camposvazquezv@gmail.com>
2020-02-04 12:46:57 -0600

    scss fix

--- web-app/src/app/app-routing.module.ts
+++ web-app/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { MobileLoginComponent } from "./mobile-login/mobile-login.component";
 import { PageProyectosComponent } from "./page-proyectos/page-proyectos.component";
+
 const routes: Routes = [
   // {path:'', component:LoginComponent},
   { path: "", component: MobileLoginComponent },
