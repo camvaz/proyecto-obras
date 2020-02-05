3602283cd8260055ec032dafe7217c6b344303db
camvaz <camposvazquezv@gmail.com>
2020-02-05 14:51:25 -0600

    Lista lista

--- web-app/src/app/home/home.component.ts
+++ web-app/src/app/home/home.component.ts
@@ -6,10 +6,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  opciones: any
 
   constructor() { }
 
   ngOnInit() {
+    this.opciones = [
+      {
+        image:"../../assets/icons/list.png",
+        name:"Proyectos"
+      },{
+        image:"../../assets/icons/calendar.png",
+        name:"Gestion Administrativa"
+      },{
+        image: "../../assets/icons/computer.png",
+        name:"Proyectos"
+      },{
+        image: "../../assets/icons/hands.png",
+        name: "Relaciones institucionales"
+      },{
+        image: "../../assets/icons/calculator.png",
+        name:"Costos"
+      }
+    ] 
+
   }
 
 }
