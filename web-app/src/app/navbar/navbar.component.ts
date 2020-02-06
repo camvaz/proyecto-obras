1be76bed8a57f42834fd46438ebd496dc892d156
Victor Campos <camposvazquezv@gmail.com>
2020-02-06 13:40:38 -0600

    hover fix

--- web-app/src/app/navbar/navbar.component.ts
+++ web-app/src/app/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +11,21 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    document.querySelector("#proyectos").addEventListener('mouseover', (event)=>{
+      document.getElementById("submenu").style.display = "flex";
+      document.getElementById("primary-nav").style.height = "100px";
+    })
+
+    document.querySelector("#submenu").addEventListener('mouseover', (event)=>{
+      document.getElementById("submenu").style.display = "flex";
+      document.getElementById("primary-nav").style.height = "100px";
+    })
+
+    document.querySelector("#submenu").addEventListener('mouseout', (event)=>{
+      document.getElementById("submenu").style.display = "none";
+
+      document.getElementById("primary-nav").style.height = "70px";
+    })
   }
 
 }
