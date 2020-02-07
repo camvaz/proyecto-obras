c3d1704de00f06b534bbc15199815f49f7675133
Victor Campos <camposvazquezv@gmail.com>
2020-02-07 07:38:48 -0600

    router links y notificacion

--- web-app/src/app/navbar/navbar.component.ts
+++ web-app/src/app/navbar/navbar.component.ts
@@ -1,29 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  selector: "app-navbar",
+  templateUrl: "./navbar.component.html",
+  styleUrls: ["./navbar.component.scss"]
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
-    document.querySelector("#proyectos").addEventListener('mouseover', (event)=>{
+    document
+      .querySelector("#proyectos")
+      .addEventListener("mouseover", event => {
+        document.getElementById("submenu").style.display = "flex";
+        document.getElementById("primary-nav").style.height = "100px";
+      });
+
+    document.querySelector("#submenu").addEventListener("mouseover", event => {
       document.getElementById("submenu").style.display = "flex";
       document.getElementById("primary-nav").style.height = "100px";
-    })
+    });
 
-    document.querySelector("#submenu").addEventListener('mouseover', (event)=>{
-      document.getElementById("submenu").style.display = "flex";
-      document.getElementById("primary-nav").style.height = "100px";
-    })
-
-    document.querySelector("#submenu").addEventListener('mouseout', (event)=>{
+    document.querySelector("#submenu").addEventListener("mouseout", event => {
       document.getElementById("submenu").style.display = "none";
       document.getElementById("primary-nav").style.height = "70px";
-    })
+    });
   }
 
+  visualizarProyectos() {
+    this.router.navigate(["/proyectos"]);
+
+    document.getElementById("submenu").style.display = "none";
+    document.getElementById("primary-nav").style.height = "70px";
+  }
+
+  crearProyecto() {
+    this.router.navigate(["/crear-proyecto"]);
+
+    document.getElementById("submenu").style.display = "none";
+    document.getElementById("primary-nav").style.height = "70px";
+  }
 }
