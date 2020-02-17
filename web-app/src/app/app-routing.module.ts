44ca7189e09f9b63e1297c39d3cba21f634331c1
Victor Campos <camposvazquezv@gmail.com>
2020-02-17 10:47:12 -0600

    feff

--- web-app/src/app/app-routing.module.ts
+++ web-app/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { PageProyectosComponent } from "./page-proyectos/page-proyectos.componen
 import { PageProyectoComponent } from "./page-proyecto/page-proyecto.component";
 import { FormCrearProyectoComponent } from "./form-crear-proyecto/form-crear-proyecto.component";
 import { VistaSubirArchivosComponent} from "./vista-subir-archivos/vista-subir-archivos.component";
-import { HomeComponent } from './home/home.component';
+// import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
 import { PerfilDepartamentoComponent } from './perfil-departamento/perfil-departamento.component';
 import { ProyectoProgramaComponent } from './proyecto-programa/proyecto-programa.component';
@@ -14,7 +14,7 @@ const routes: Routes = [
   // {path:'', component:LoginComponent},
   { path: "", component: MobileLoginComponent },
   { path: "search/:string", component: SearchComponent },
-  { path: "proyectos", component: PageProyectosComponent },
+  { path: "proyectos", component: ProyectoProgramaComponent },
   { path: "proyecto", component: PageProyectoComponent },
   { path: "crear-proyecto", component: FormCrearProyectoComponent },
   { path: "perfil-departamentos", component: PerfilDepartamentoComponent},
